3945511efef1a0d3271e3c852129cd2e9d3f916e
websitreon <websitreon@gmail.com>
2026-09-12 23:35:24 +0100

    Delete worker.js

--- worker.js
+++
@@ -1,2 +0,0 @@
-import { handleApi } from './functions/_lib/api.js';
-export default { async fetch(request,env,ctx){ const api=await handleApi(request,env); if(api)return api; return env.ASSETS.fetch(request); } };
